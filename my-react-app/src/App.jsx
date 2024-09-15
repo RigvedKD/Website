470d5034987debba1c5fbe5a78b72563159bcabf
rdoddiBRK <rdoddi@brocksolutions.com>
2024-09-15 10:16:42 -0400

    Lesson 3 css styling

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,14 +1,7 @@
-import Header from './Header.jsx'
-import Footer from './Footer.jsx' 
-import Food   from './Food.jsx'
+import Button from './Button.jsx'
 function App() {
   return(
-    <>
-    <Header></Header>
-    <Food></Food>
-    <Food/>
-    <Footer></Footer>
-    </>
+   <><Button></Button></>
   )
 }
 
