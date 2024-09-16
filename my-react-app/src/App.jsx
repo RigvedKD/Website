e2548dacce3e3b4de9077fb0af2eef17456026ad
rdoddiBRK <rdoddi@brocksolutions.com>
2024-09-16 17:56:17 -0400

    render list

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,13 +1,23 @@
-import Header from './Header.jsx'
-import Footer from './Footer.jsx' 
-import Food   from './Food.jsx'
+import List from './List'
 function App() {
+  const fruits =[
+    {id: 0, name: "apple", calories: 95}, 
+    {id: 1, name: "orange", calories: 45}, 
+    {id: 2, name: "banana", calories: 105}, 
+    {id: 3, name: "coconut", calories: 159}, 
+    {id: 4, name: "pineapple",  calories: 37}];
+
+    const Veg =[
+      {id: 0, name: "potatoes", calories: 123}, 
+      {id: 1, name: "carrots", calories: 321}, 
+      {id: 2, name: "corn", calories: 213}, 
+      {id: 3, name: "broccolli", calories: 151239}, 
+      {id: 4, name: "celery",  calories: 123}];
   return(
     <>
-    <Header></Header>
-    <Food></Food>
-    <Food/>
-    <Footer></Footer>
+    {fruits.length > 0 && <List items={fruits} category="Fruits"></List> }
+    <List items={Veg} category="Vegatbles"></List>
+    <List></List>
     </>
   )
 }
